4bebd55f6ddafab1e347440d09e5b817b4c0f437
swathynishad <swathyps1997m10@gmail.com>
2019-04-01 11:24:48 +0530

    new Echo server pgm committed

--- Client.java
+++ Client.java
@@ -1,69 +1,20 @@
-import java.net.*; 
-import java.io.*; 
-  
-public class Client 
-{ 
-    // initialize socket and input output streams 
-    private Socket socket            = null; 
-    private DataInputStream  input   = null; 
-    private DataOutputStream out     = null; 
-  
-    // constructor to put ip address and port 
-    public Client(String address, int port) 
-    { 
-        // establish a connection 
-        try
-        { 
-            socket = new Socket(address, port); 
-            System.out.println("Connected"); 
-  
-            // takes input from terminal 
-            input  = new DataInputStream(System.in); 
-  
-            // sends output to the socket 
-            out    = new DataOutputStream(socket.getOutputStream()); 
-        } 
-        catch(UnknownHostException u) 
-        { 
-            System.out.println(u); 
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
-  
-        // string to read message from input 
-        String line = ""; 
-  
-        // keep reading until "Over" is input 
-        while (!line.equals("Over")) 
-        { 
-            try
-            { 
-                line = input.readLine(); 
-                out.writeUTF(line); 
-            } 
-            catch(IOException i) 
-            { 
-                System.out.println(i); 
-            } 
-        } 
-  
-        // close the connection 
-        try
-        { 
-            input.close(); 
-            out.close(); 
-            socket.close(); 
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
+    import java.net.*; 
+    import java.io.*;  
+    class Client{  
+    public static void main(String args[])throws Exception{  
+    Socket s=new Socket("localhost",3334); 
+    System.out.println("Connected to Server"); 
+    DataInputStream din=new DataInputStream(s.getInputStream());  
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+    String str="",str2="";  
+    while(!str.equals("stop")){  
+    str=br.readLine();  
+    dout.writeUTF(str);  
+    dout.flush();  
+    str2=din.readUTF();  
+    System.out.println(str2);  
     } 
-  
-    public static void main(String args[]) 
-    { 
-        Client client = new Client("127.0.0.1", 5000); 
-    } 
-}
+    dout.close();  
+    s.close();  
+    }}  
--- Server.java
+++ Server.java
@@ -1,61 +1,23 @@
-import java.net.*; 
-import java.io.*; 
-  
-public class Server 
-{ 
-    //initialize socket and input stream 
-    private Socket          socket   = null; 
-    private ServerSocket    server   = null; 
-    private DataInputStream in       =  null; 
-  
-    // constructor with port 
-    public Server(int port) 
-    { 
-        // starts server and waits for a connection 
-        try
-        { 
-            server = new ServerSocket(port); 
-            System.out.println("Server started"); 
-  
-            System.out.println("Waiting for a client ..."); 
-  
-            socket = server.accept(); 
-            System.out.println("Client accepted"); 
-  
-            // takes input from the client socket 
-            in = new DataInputStream( 
-                new BufferedInputStream(socket.getInputStream())); 
-  
-            String line = ""; 
-  
-            // reads message from client until "Over" is sent 
-            while (!line.equals("Over")) 
-            { 
-                try
-                { 
-                    line = in.readUTF(); 
-                    System.out.println(line); 
-  
-                } 
-                catch(IOException i) 
-                { 
-                    System.out.println(i); 
-                } 
-            } 
-            System.out.println("Closing connection"); 
-  
-            // close connection 
-            socket.close(); 
-            in.close(); 
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
-    } 
-  
-    public static void main(String args[]) 
-    { 
-        Server server = new Server(5000); 
-    } 
-} 
+    import java.net.*;  
+    import java.io.*;  
+    class Server{  
+    public static void main(String args[])throws Exception{ 
+    System.out.println("Waiting for connection"); 
+    ServerSocket ss=new ServerSocket(3334);  
+    Socket s=ss.accept();
+    System.out.println("Connected");  
+    DataInputStream din=new DataInputStream(s.getInputStream());  
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+    String str="",str2="";  
+    while(!str.equals("stop")){  
+    str=din.readUTF();     
+    dout.writeUTF(str.toUpperCase());  
+    dout.flush();  
+    }  
+    din.close();  
+    s.close();  
+    ss.close();  
+    }}  
+
+
